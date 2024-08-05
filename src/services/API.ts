6d57df9ef1448c7bee3bba7eccfdd38d1a91a68b
Alvaro Marinho <alvaromarinho@Alvaros-MacBook-Air.local>
2024-08-05 09:51:10 -0300

    axios no-cache

--- src/services/API.ts
+++ src/services/API.ts
@@ -4,8 +4,10 @@ import axios from 'axios';
 export const apiCtx = (ctx?: any) => {
     const api = axios.create({ baseURL: process.env.API_URL });
     const { akToken } = parseCookies(ctx);
-    if (akToken) 
+    if (akToken) {
         api.defaults.headers['Authorization'] = `Bearer ${akToken}`;
+        api.defaults.headers['Cache-Control'] = 'no-cache';
+    }
 
     return api;
 }
